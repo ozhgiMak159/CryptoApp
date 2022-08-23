ff0d6d0b9fa0a7b8fc50a398e2a3f302fe586822
Maksim Ozhgibecov <93679282+ozhgiMak159@users.noreply.github.com>
2022-08-23 10:16:34 +0500

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Maksim  on 23.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
