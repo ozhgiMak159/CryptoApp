784cb8e8f7bb536746b7711e435460ac28bc0b46
Maksim Ozhgibecov <93679282+ozhgiMak159@users.noreply.github.com>
2022-08-23 11:56:28 +0500

    Start

--- CryptoApp/ContentView.swift
+++ CryptoApp/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack(spacing: 40) {
+                Text("Accent Color")
+                    .foregroundColor(Color.theme.accent)
+                
+                Text("Secondary Text Color")
+                    .foregroundColor(Color.theme.secondaryTextColor)
+                
+                Text("Red Color")
+                    .foregroundColor(Color.theme.redColor)
+                
+                Text("Green Color")
+                    .foregroundColor(Color.theme.greenColor)
+            }
+            .font(.headline)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            
     }
 }
---
+++ CryptoApp/Extensions/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Maksim  on 23.08.2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let greenColor = Color("GreenColor")
+    let redColor = Color("RedColor")
+    let secondaryTextColor = Color("SecondaryTextColor")
+
+}
